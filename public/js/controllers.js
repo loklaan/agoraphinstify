e10885ea4fddc3d18238b652cb31a6a0da83681e
Lochlan Bunn <loklaan@gmail.com>
2014-08-19 18:01:52 +1000

    Refactored controller code #13

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,29 +8,46 @@
     'leaflet-directive'
     ]);
 
-  module.controller('MapController', ['$scope', 'leafletData', function($scope, leafletData) {
+  /**
+   * Map Controller
+   * Handles updating the Map with new data.
+   */
+  module.controller('MapController', [
+    '$scope',
+    'leafletData',
+    'EventSearch',
+  function($scope, leafletData, EventSearch) {
 
     // MAP DEFAULTS
-    $scope.default = {
-      zoomControl: false
-    };
-    $scope.center = {
-      autoDiscover: true,
-      zoom: 14
-    };
-    $scope.tiles = {
-      url: "http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg",
-      options: {
-        attribution: '&copy; <a href="http://stamen.com">Stamen Design</a> Tile Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
-      }
-    };
+    angular.extend($scope, {
+      default: {
+        zoomControl: false
+      },
+      center: {
+        autoDiscover: true,
+        zoom: 14
+      },
+      tiles: {
+        url: "http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg",
+        options: {
+          attribution: '&copy; <a href="http://stamen.com">Stamen Design</a> Tile Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors'
+        }
+      },
+      markers: {}
+    });
 
     leafletData.getMap().then(function(map) {
       // do stuff with map object
     });
+
   }]);
 
-  module.controller('InstifyController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  module.controller('InstifyController', [
+    '$scope',
+    '$routeParams',
+  function($scope, $routeParams) {
+
+
 
   }]);
 
