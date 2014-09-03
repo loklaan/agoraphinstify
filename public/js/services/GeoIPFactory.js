3784bd5de5b55b72e369a941b17387616c8a8d5b
Lochlan Bunn <loklaan@gmail.com>
2014-09-03 23:32:00 +1000

    Added GeoIP ng service to ask Telize for IP info

---
+++ public/js/services/GeoIPFactory.js
@@ -0,0 +1,66 @@
+/**
+ * Telize API Service
+ *
+ * Responsible for getting the geo ip information of the User.
+ * This information is used solely for determining the Country
+ * Code of the visitingg User. Only the Country Code will be
+ * transported to the server.
+ */
+
+(function(_) {
+
+  var module = angular.module('AgoraApp');
+
+  module.factory('GeoIP', [
+    '$resource',
+  function($resource) {
+
+/* ==========================================================================
+   Factory Variables
+   ========================================================================== */
+
+    var API = 'http://www.telize.com/geoip';
+
+    var GeoIPFactory = {},
+        _geoip = null;
+
+
+/* ==========================================================================
+   Factory Public Functions
+   ========================================================================== */
+
+    GeoIPFactory.getVisitorGeoIP = function() {
+      GeoIPLocation.get(
+        // Success API call
+        function(data) {
+          _geoip = data;
+        },
+        // Failed API call
+        function(reason) {
+          console.error(reason);
+        }
+      );
+    };
+
+    GeoIPFactory.getCountryCode = function() {
+      return _geoip.country_code;
+    };
+
+
+/* ==========================================================================
+   REST Client Functions
+   ========================================================================== */
+
+    var GeoIPLocation = $resource(API, {},
+      {
+        get: {
+          method: 'GET'
+        }
+      });
+
+
+    return GeoIPFactory;
+
+  }]);
+
+})(_);
